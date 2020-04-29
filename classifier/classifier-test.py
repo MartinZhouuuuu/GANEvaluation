be3d401abfb8ef10182b2bedf5416ab9625181a3
Lenovo <zhoulele12@gmail.com>
2020-04-29 21:48:51 +0800

    modified val and test dataloading

--- classifier/classifier-test.py
+++ classifier/classifier-test.py
@@ -17,17 +17,10 @@ if __name__ == '__main__':
 	batch_size = 64
 	dataset_size = 20000
 
-
 	transform = transforms.Compose(
 			[
 				transforms.Resize(256,Image.LANCZOS),
 				transforms.CenterCrop(256),
-				transforms.ToTensor(),
-				transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True),
-			]
-		)
-	transform_generated = transforms.Compose(
-			[
 				transforms.ToTensor(),
 				transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True),
 			]
@@ -48,25 +41,24 @@ if __name__ == '__main__':
 		train,
 		shuffle = True, 
 		batch_size = batch_size,
-		num_workers = 8)
+		num_workers = 8,
+		pin_memory=True)
 
 	valLoader = DataLoader(
 		val,
 		shuffle = False, 
-		batch_size = 2000,
-		num_workers = 8)
+		batch_size = 64,
+		num_workers = 8,
+		pin_memory=True)
 
-	val_images, val_labels = iter(valLoader).next()
-	val_images, val_labels = val_images.to('cuda'), val_labels.type(torch.cuda.FloatTensor).to('cuda')
-
-	testLoader = DataLoader(test,shuffle = False, batch_size = 2000)
+	testLoader = DataLoader(test,shuffle = False, batch_size = 64)
 
 	#network
-	classifier = FCNET(3*2**16, [20], 1).to('cuda')
-	# classifier = ConvNet([16,16,16],include_dense=True).cuda()
+	# classifier = FCNET(3*2**16, [20,20,20,20,20,20,20], 1).to('cuda')
+	classifier = ConvNet([16,16,16,16],include_dense=True).to('cuda')
 	print(classifier)
 	loss_criterion = nn.BCELoss()
-	adam_optim = optim.Adam(classifier.parameters())
+	adam_optim = optim.Adam(classifier.parameters(), lr=0.001)
 
 	train_epoch_loss = []
 	val_epoch_loss = []
@@ -75,7 +67,7 @@ if __name__ == '__main__':
 	highest_val_acc = 0
 
 	train = True
-
+	
 	if train:
 		for epoch in range(num_epochs):
 
@@ -87,7 +79,7 @@ if __name__ == '__main__':
 				iteration_count += 1
 				#torch.cuda is really crucial
 				train_images, train_labels = batch[0].to('cuda'), batch[1].type(torch.cuda.FloatTensor).to('cuda')
-				
+
 				adam_optim.zero_grad()
 
 				train_pred = classifier(train_images).squeeze(1)
@@ -109,6 +101,7 @@ if __name__ == '__main__':
 						train_correct += 1
 
 				print('iteration', i+1 ,'train_loss %.3f' % loss.item())
+				del train_images,train_labels
 
 			train_loss = running_loss/iteration_count
 			train_acc = train_correct / train_size  * 100
@@ -119,14 +112,40 @@ if __name__ == '__main__':
 
 			with torch.no_grad():
 				classifier.eval()
+				val_running_loss = 0
+				val_iteration_count = 0
+				val_correct = 0
 
-				val_pred = classifier(val_images).squeeze(1)
-				val_loss = loss_criterion(val_pred, val_labels).item()
-				val_acc = accuracy(val_pred, val_labels,val_size)
+				for i,batch in enumerate(valLoader):
+
+					val_images, val_labels = batch[0].to('cuda'), batch[1].type(torch.cuda.FloatTensor).to('cuda')
+					
+					val_pred = classifier(val_images).squeeze(1)
+
+					val_iteration_loss = loss_criterion(val_pred, val_labels)
+					val_running_loss += val_iteration_loss.item()
+
+					for index in range(val_labels.size()[0]):
+						if val_pred[index]>=0.5:
+							val_pred[index] = 1
+						else:
+							val_pred[index] = 0
+						
+						if int(val_pred[index]) == int(val_labels[index]):
+							val_correct += 1
+
+					val_iteration_count += 1
+
+				val_loss = val_running_loss / val_iteration_count
+				
+				val_acc = val_correct / val_size * 100
+
 				if val_acc > highest_val_acc:
 					highest_val_acc = val_acc
-					torch.save(classifier.state_dict(), 'model-files/%d-%.2f.pth'%(epoch,val_acc))
+					#epochs of model files are wrong
+					torch.save(classifier.state_dict(), 'model-files/%d-%.2f.pth'%(epoch+1,val_acc))
 					print('new model saved')
+				
 				print('Epoch', epoch+1, 'val_loss','%.2f'%val_loss,'val_acc', '%.2f'%val_acc + '%')
 				
 				val_epoch_loss.append(val_loss)
@@ -139,13 +158,33 @@ if __name__ == '__main__':
 	with torch.no_grad():
 		classifier.eval()
 		if not train:
-			classifier.load_state_dict(
-				torch.load('model-files/60-91.85.pth')
-				)
-		test_images,test_labels = iter(testLoader).next()
-		test_images, test_labels = test_images.to('cuda'), test_labels.type(torch.cuda.FloatTensor).to('cuda')
-		test_pred = classifier(test_images).squeeze(1)
+			classifier.load_state_dict(torch.load('model-files/28-93.65.pth'))
+			test_running_loss = 0
+			test_iteration_count = 0
+			test_correct = 0
 
-		test_loss = loss_criterion(test_pred, test_labels).item()
-		test_acc = accuracy(test_pred, test_labels,test_size)
-		print('test_loss','%.2f'%test_loss,'test_acc', '%.2f'%test_acc + '%')
+			for i,batch in enumerate(testLoader):
+
+				test_images, test_labels = batch[0].to('cuda'), batch[1].type(torch.cuda.FloatTensor).to('cuda')
+				
+				test_pred = classifier(test_images).squeeze(1)
+
+				test_iteration_loss = loss_criterion(test_pred, test_labels)
+				test_running_loss += test_iteration_loss.item()
+
+				for index in range(test_labels.size()[0]):
+					if test_pred[index]>=0.5:
+						test_pred[index] = 1
+					else:
+						test_pred[index] = 0
+					
+					if int(test_pred[index]) == int(test_labels[index]):
+						test_correct += 1
+
+				test_iteration_count += 1
+
+			test_loss = test_running_loss / test_iteration_count
+			
+			test_acc = test_correct / test_size * 100
+	
+			print('test_loss','%.2f'%test_loss,'test_acc', '%.2f'%test_acc + '%')
