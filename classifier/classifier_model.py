corrected normalization error, switched to softmax

classifier/classifier_model.py
@@ -11,21 +11,20 @@ class FCNET(nn.Module):
 		self.input_dim = input_dim
 		self.num_hidden_layer = len(num_units)
 		self.progression = nn.ModuleList([])
-		# self.progression.append(nn.Linear(input_dim,out_dim))
+		self.progression.append(nn.Linear(input_dim,out_dim))
 		# self.progression.append(nn.Sigmoid())
-		
-		self.progression.append(nn.Linear(self.input_dim,num_units[0]))
-		self.progression.append(nn.ReLU())
+
+		# self.progression.append(nn.Linear(self.input_dim,num_units[0]))
+		# self.progression.append(nn.ReLU())
 		
 		for i in range(self.num_hidden_layer-1):
 			self.progression.append(nn.Linear(num_units[i],num_units[i+1]))
 			self.progression.append(nn.ReLU())
 		
-		self.progression.append(nn.Linear(num_units[-1],out_dim))
+		# self.progression.append(nn.Linear(num_units[-1],out_dim))
 
-		self.progression.append(nn.Sigmoid())
+		# self.progression.append(nn.Sigmoid())
 		
-		# self.progression.append(nn.Softmax())
 
 	def forward(self,x):
 		x = x.view(-1,self.input_dim)

classifier/utility.py
@@ -6,7 +6,9 @@ import torch
 import torch.nn.functional as F
 
 def count_correct(pred,labels):
+	
 	correct = 0
+	'''
 	class_prediction = pred.clone()
 	for i in range(pred.size()[0]):
 		if pred[i]>=0.5:
@@ -15,8 +17,13 @@ def count_correct(pred,labels):
 			class_prediction[i] = 0
 		if int(class_prediction[i]) == int(labels[i]):
 			correct += 1
-
-	return class_prediction,correct
+	'''
+	#returns the indices of the larger values
+	_,predicted = torch.max(pred,1)
+	# print(predicted)
+	correct += (labels == predicted).sum().item()
+	# print(predicted)
+	return predicted,correct
 
 def plot_loss(train_loss,val_loss,path):
 	plt.plot(list(range(1, len(train_loss)+1)),train_loss)
@@ -66,6 +73,18 @@ def predict_and_save(images,predictions,labels,classes,batch_num):
 	class_prediction = count_correct(predictions, labels)[0]
 	for idx in range(images.size()[0]):
 		matplotlib_imshow(images[idx])
+		plt.title("{0}, \n(label: {1})".format(
+			classes[int(class_prediction[idx])],
+			# predictions[idx],
+			classes[int(labels[idx])]),
+					color=("green" if int(class_prediction[idx])==int(labels[idx]) else "red"))
+		file_name = batch_num*64 + idx
+		flag = str(int(class_prediction[idx])==int(labels[idx]))
+		plt.savefig('constant1n2/%s-%.3f-%d.jpg' % (flag,class_prediction[idx],file_name))
+		plt.close()
+	'''
+	for idx in range(images.size()[0]):
+		matplotlib_imshow(images[idx])
 		plt.title("{0}, {1:.3f}\n(label: {2})".format(
 			classes[int(class_prediction[idx])],
 			predictions[idx],
@@ -75,3 +94,4 @@ def predict_and_save(images,predictions,labels,classes,batch_num):
 		flag = str(int(class_prediction[idx])==int(labels[idx]))
 		plt.savefig('pred/%s-%.3f-%d.jpg' % (flag,predictions[idx],file_name))
 		plt.close()
+		'''